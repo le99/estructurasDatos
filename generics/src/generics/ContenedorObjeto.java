21c6fef638edd683b07a46ab6b12a4138cdd5e7c
Luis Florez <platestopapp@gmail.com>
2018-08-08 11:27:54 -0500

    Cast sencillo

---
+++ generics/src/generics/ContenedorObjeto.java
@@ -0,0 +1,14 @@
+package generics;
+
+
+public class ContenedorObjeto {
+	private Object valor;
+	
+	public Object get() {
+		return valor;
+	}
+	
+	public void set(Object valor) {
+		this.valor = valor;
+	}
+}
--- generics/src/generics/Main.java
+++ generics/src/generics/Main.java
@@ -1,9 +1,31 @@
 package generics;
 
+/**
+ * Para más documentación mirar:
+ * https://docs.oracle.com/javase/tutorial/java/generics/index.html
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		System.out.println("-------------------------------");
+		System.out.println("Uso de un contenedor de objetos");
+		System.out.println("-------------------------------");
+
+		ContenedorObjeto c = new ContenedorObjeto();
+		c.set("Algun contenido");
+		//Es necesario usar un cast cuando queramos recuperar los objetos
+		String valor = (String)c.get();
+		System.out.println("Cast exitoso: " + valor);
+		
+		try {
+			int v = (int)c.get();
+		}
+		catch (Exception e){
+			System.out.println("El cast Falló:");
+			e.printStackTrace();
+		}
 		
 	}
 
